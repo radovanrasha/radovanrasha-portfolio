82db721baaffab4563f206473c0552058fac89da
radovanrasha <kking1755@gmail.com>
2024-09-08 13:13:45 +0200

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: 'portfolio',
-      script: 'npm run start:prod',
+      name: "portfolio",
+      script: "npm run start:prod",
       port: 3000,
       time: true,
     },
   ],
   deploy: {
     production: {
-      user: 'dev',
-      host: '161.97.65.149',
-      key: 'radovanrasha.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:radovanrasha/radovanrasha-portfolio.git',
-      path: '/home/dev/portfolio',
+      user: "dev",
+      host: "161.97.65.149",
+      key: "radovanrasha.pem",
+      ref: "origin/master",
+      repo: "git@github.com:radovanrasha/radovanrasha-portfolio.git",
+      path: "/home/dev/portfolio",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
-      'post-deploy':
-        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      "post-deploy":
+        "rm -rf node_modules && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
     },
   },
 };
